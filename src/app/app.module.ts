6ff9a665d97060ab80d75a3558d20b80e26b9faa
ashwindaniel <d.redvirus@gmail.com>
2017-10-04 18:06:23 +0530

    FoodBucket init

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+import { CartService } from './services/cart.service';
+import { DataService } from './services/data.service';
+
+const routeConfig: Routes = [
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  {
+    path: 'menu',
+    component: MenuComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    MenuComponent,
+    CartComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routeConfig)
+  ],
+  providers: [CartService, DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
